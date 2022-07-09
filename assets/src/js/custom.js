4bf422e05798a995e363a17f96342e672a832789
Evgeniy Beskrovnyy <e.beskrovnyy@gmail.com>
2022-07-10 00:21:45 +0300

    added open-close js logic and animation

--- assets/src/js/custom.js
+++ assets/src/js/custom.js
@@ -1,3 +1,24 @@
 window.addEventListener('DOMContentLoaded', function() {
     document.getElementById('svg-icons').innerHTML = SVG_SPRITE;
+
+    toggleMobileMenu();
+
+    function toggleMobileMenu() {
+        console.log('xxx');
+        const buttons = document.querySelectorAll('[data-toggle-menu]');
+        const page = document.getElementsByTagName('body')[0];
+        const openState = 'is-menu-open';
+
+        document.addEventListener('keyup', (event) => {
+            if (event.code.toUpperCase() === 'ESCAPE') {
+                page.classList.remove(openState);
+            }
+        });
+
+        buttons.forEach((el) => {
+            el.addEventListener('click', () => {
+                page.classList.toggle(openState);
+            });
+        });
+    }
 })
